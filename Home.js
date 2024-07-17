6b244956c5fd8ca691c42b8029776f490919053c
KarolLisset <70411448+KarolLisset@users.noreply.github.com>
2024-07-16 20:18:09 -0600

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenido al Repositorio de Videos de YouTube</h1>
+      <p>Explora nuestras categorías y videos.</p>
+    </div>
+  );
+}
+
+export default Home;
