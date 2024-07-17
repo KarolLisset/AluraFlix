2399f7f59d99124d5d0f954521c02c550c29f179
KarolLisset <70411448+KarolLisset@users.noreply.github.com>
2024-07-16 20:22:36 -0600

    Update App.js

--- App.js
+++ App.js
@@ -3,24 +3,30 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Home from './Pages/Home';
 import Categories from './Pages/Categories';
 import Videos from './Pages/Videos';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
-          <ul>
-            <li><Link to="/">Inicio</Link></li>
-            <li><Link to="/categories">Categorías</Link></li>
-            <li><Link to="/videos">Videos</Link></li>
-          </ul>
+        <nav className="navbar">
+          <div className="container">
+            <h1 className="logo">YouTube Repository</h1>
+            <ul className="nav-links">
+              <li><Link to="/">Inicio</Link></li>
+              <li><Link to="/categories">Categorías</Link></li>
+              <li><Link to="/videos">Videos</Link></li>
+            </ul>
+          </div>
         </nav>
 
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/categories" component={Categories} />
-          <Route path="/videos" component={Videos} />
-        </Switch>
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/categories" component={Categories} />
+            <Route path="/videos" component={Videos} />
+          </Switch>
+        </div>
       </div>
     </Router>
   );
